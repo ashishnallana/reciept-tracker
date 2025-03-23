bccb80ece468e0431f2b7f54bd760cd246ecb21b
Ashish Nallana <ashishnallana@gmail.com>
2025-03-23 14:09:55 +0530

    add Schematic integration and temporary access token functionality

--- components/ConvexClientProvider.tsx
+++ components/ConvexClientProvider.tsx
@@ -1,10 +1,46 @@
 "use client";
 
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
+import React from "react";
 import { ConvexReactClient } from "convex/react";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
-import { useAuth } from "@clerk/nextjs";
+import { useAuth, useUser } from "@clerk/nextjs";
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+import {
+  SchematicProvider,
+  useSchematicEvents,
+} from "@schematichq/schematic-react";
+
+const SchematicWrapped = ({ children }: { children: React.ReactNode }) => {
+  const { identify } = useSchematicEvents();
+  const { user } = useUser();
+  useEffect(() => {
+    const username =
+      user?.username ??
+      user?.fullName ??
+      user?.emailAddresses[0]?.emailAddress ??
+      user?.id;
+
+    if (user?.id) {
+      identify({
+        name: username,
+        // user level keys
+        keys: {
+          id: user.id,
+        },
+        // company level keys
+        company: {
+          keys: {
+            id: user.id, // we are using the same user id, because we are building a B2C app
+          },
+          name: username,
+        },
+      });
+    }
+  }, [user, identify]);
+
+  return children;
+};
 
 export default function ConvexClientProvider({
   children,
@@ -13,7 +49,11 @@ export default function ConvexClientProvider({
 }) {
   return (
     <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
-      {children}
+      <SchematicProvider
+        publishableKey={`${process.env.NEXT_PUBLIC_SCHEMATIC_KEY}`}
+      >
+        <SchematicWrapped>{children}</SchematicWrapped>
+      </SchematicProvider>
     </ConvexProviderWithClerk>
   );
 }
